c1b6868ee4ab69930bcbb17cf3a3861513c7a4cb
Kenny Vilella <kennyvilella@gmail.com>
2023-07-16 14:00:01 +0900

    [added] header file for soil_dynamics.cpp

---
+++ src/soil_dynamics.hpp
@@ -0,0 +1,16 @@
+/*
+
+*/
+#ifndef SOIL_DYNAMICS_H
+#define SOIL_DYNAMICS_H
+
+namespace soil_simulator
+{
+    class soil_dynamics
+    {
+    public:
+        void init();
+        void step();
+    };
+}
+#endif
