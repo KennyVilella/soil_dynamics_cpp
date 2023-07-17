dad78e0e8b75d62d2356365b0b97a7daf0bdd0c4
Kenny Vilella <kennyvilella@gmail.com>
2023-07-17 11:16:03 +0900

    [added] all types declaration in types.h

--- src/types.hpp
+++ src/types.hpp
@@ -12,6 +12,7 @@ namespace soil_simulator
     class grid : public soil_dynamics
     {
     public:
+        /* Class member */
         int half_length_x;
         int half_length_y;
         int half_length_z;
@@ -23,17 +24,63 @@ namespace soil_simulator
         std::vector<float> vect_y;
         std::vector<float> vect_z;
 
+        /* Constructor */
         grid(
             float grid_size_x = 4.0, float grid_size_y = 4.0, float grid_size_z = 4.0,
             float cell_size_xy = 0.05, float cell_size_z = 0.01
-        ); /* constructor */
+        );
     };
 
     class bucket : public soil_dynamics
-    {};
+    {
+    public:
+        /* Class member */
+        std::vector<float> j_pos_init;
+        std::vector<float> b_pos_init;
+        std::vector<float> t_pos_init;
+        float width;
+        std::vector<float> pos;
+        std::vector<float> ori;
+
+        /* Constructor */
+        bucket(
+            std::vector<float> o_pos_init = {0.0, 0.0, 0.0},
+            std::vector<float> j_pos_init = {0.0, 0.0, 0.0},
+            std::vector<float> b_pos_init = {0.0, 0.0, -0.5},
+            std::vector<float> t_pos_init = {0.7, 0.0, -0.5},
+            float width = 0.5
+        );
+    };
+
     class sim_param : public soil_dynamics
-    {};
+    {
+    public:
+        /* Class member */
+        float repose_angle;
+        int max_iterations;
+        int cell_buffer;
+
+        /* Constructor */
+        sim_param(
+            float repose_angle = 0.85, int max_iterations = 10, int cell_buffer = 4
+        );
+    };
+
     class sim_out : public soil_dynamics
-    {};
+    {
+    public:
+        /* Class member */
+        bool equilibrium;
+        float terrain;
+        float body;
+        float body_soil;
+        std::vector<std::vector<int>> body_soil_pos;
+        int bucket_area;
+        int relax_area;
+        int impact_area;
+
+        /* Constructor */
+        sim_out(int terrain, grid grid);
+    };
 }
 #endif /*TYPES_H*/
