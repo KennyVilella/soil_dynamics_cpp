1c9e0b9189002a81f915c303b6dc6200e84a0563
Kenny Vilella <kennyvilella@gmail.com>
2023-07-25 09:50:00 +0900

    [added] dummy unit tests for bucket_pos

---
+++ test/unit_tests/test_bucket_pos.cpp
@@ -0,0 +1,15 @@
+/*
+This file implements unit tests for the functions in bucket_pos.cpp.
+
+Copyright, 2023, Vilella Kenny.
+*/
+#include "gtest/gtest.h"
+#include "src/bucket_pos.cpp"
+
+TEST(UnitTestBucketPos, CalcLinePos) {
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
