c2e98c5069e2afb9b22fb5f05a1aaf278a26032b
Kenny Vilella <kennyvilella@gmail.com>
2023-07-31 10:25:43 +0900

    [added] dummy docstrings in soil_dynamics.hpp

--- src/soil_dynamics.hpp
+++ src/soil_dynamics.hpp
@@ -7,11 +7,19 @@ Copyright, 2023, Vilella Kenny.
 
 namespace soil_simulator {
 
+/// \brief Simulation class.
 class SoilDynamics {
  public:
+     /// \brief Initialize the simulator.
      void init();
+
+     /// \brief Step the simulation.
      void step();
+
+     /// \brief Check the validity of the simulation outputs.
      void check();
+
+     /// \brief Write the simulation outputs into files.
      void WriteOutputs();
 };
 }  // namespace soil_simulator
