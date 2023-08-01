8d344324d97eb3044a4e107d97349a813fee93bb
Kenny Vilella <kennyvilella@gmail.com>
2023-08-01 09:27:57 +0900

    [added] skeleton for unit tests of intersecting_cells

---
+++ test/unit_tests/test_intersecting_cells.cpp
@@ -0,0 +1,19 @@
+/*
+This file implements unit tests for the functions in intersecting_cells.cpp.
+
+Copyright, 2023, Vilella Kenny.
+*/
+#include "gtest/gtest.h"
+#include "src/intersecting_cells.cpp"
+
+TEST(UnitTestIntersectingCells, MoveBodySoil) {
+}
+
+TEST(UnitTestIntersectingCells, MoveIntersectingBodySoil) {
+}
+
+TEST(UnitTestIntersectingCells, LocateIntersectingCells) {
+}
+
+TEST(UnitTestIntersectingCells, MoveIntersectingBody) {
+}
