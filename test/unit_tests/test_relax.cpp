bf1ce4c62a696452014d0da4b85c5d738b4bdbfc
Kenny Vilella <kennyvilella@gmail.com>
2023-08-07 08:55:39 +0900

    [added] skeleton for unit tests of relax functions

---
+++ test/unit_tests/test_relax.cpp
@@ -0,0 +1,28 @@
+/*
+This file implements unit tests for the functions in relax.cpp.
+
+Copyright, 2023, Vilella Kenny.
+*/
+#include "gtest/gtest.h"
+#include "src/relax.cpp"
+
+TEST(UnitTestRelax, LocateUnstableTerrainCell) {
+}
+
+TEST(UnitTestRelax, CheckUnstableTerrainCell) {
+}
+
+TEST(UnitTestRelax, RelaxUnstableTerrainCell) {
+}
+
+TEST(UnitTestRelax, RelaxTerrain) {
+}
+
+TEST(UnitTestRelax, CheckUnstableBodyCell) {
+}
+
+TEST(UnitTestRelax, RelaxUnstableBodyCell) {
+}
+
+TEST(UnitTestRelax, RelaxBodySoil) {
+}
