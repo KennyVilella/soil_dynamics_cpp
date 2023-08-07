6b10a783d47a5136e1396dd9d0061db126e1c040
Kenny Vilella <kennyvilella@gmail.com>
2023-08-07 09:54:00 +0900

    [added] preliminary version of the CheckUnstableTerrainCell function

--- src/relax.cpp
+++ src/relax.cpp
@@ -55,6 +55,112 @@ std::vector<std::vector<int>> soil_simulator::LocateUnstableTerrainCell(
 int soil_simulator::CheckUnstableTerrainCell(
     SimOut* sim_out, int ii_c, int jj_c, float h_min, float tol
 ) {
+    // Checking whether soil is low enough, unstability may have already
+    // been solved
+    if (sim_out->terrain_[ii_c][jj_c] + tol < h_min) {
+        // Adjacent terrain is low enough
+        bool bucket_presence_1 = (
+            (sim_out->body_[0][ii_c][jj_c] != 0.0) ||
+            (sim_out->body_[1][ii_c][jj_c] != 0.0));
+        bool bucket_presence_3 = (
+            (sim_out->body_[2][ii_c][jj_c] != 0.0) ||
+            (sim_out->body_[3][ii_c][jj_c] != 0.0));
+        int status;
+        float column_top;
+        float bucket_bot;
+
+        if (bucket_presence_1 || bucket_presence_3) {
+            // Bucket is present
+            // Calculating extension of bucket and soil
+            if (!bucket_presence_1) {
+                // Only the second bucket layer is present
+                status = 200;
+                bucket_bot = sim_out->body_[2][ii_c][jj_c];
+                if (
+                    (sim_out->body_soil_[2][ii_c][jj_c] != 0.0) ||
+                    (sim_out->body_soil_[3][ii_c][jj_c] != 0.0)
+                ) {
+                    // Bucket soil is present
+                    status += 10;
+                    column_top = sim_out->body_soil_[3][ii_c][jj_c];
+                } else {
+                    // Bucket soil is not present
+                    status += 20;
+                    column_top = sim_out->body_[3][ii_c][jj_c];
+                }
+            } else if (!bucket_presence_3) {
+                // Only the first bucket layer is present
+                status = 100;
+                bucket_bot = sim_out->body_[0][ii_c][jj_c];
+                if (
+                    (sim_out->body_soil_[0][ii_c][jj_c] != 0.0) ||
+                    (sim_out->body_soil_[1][ii_c][jj_c] != 0.0)
+                ) {
+                    // Bucket soil is present
+                    status += 30;
+                    column_top = sim_out->body_soil_[1][ii_c][jj_c];
+                } else {
+                    // Bucket soil is not present
+                    status += 40;
+                    column_top = sim_out->body_[1][ii_c][jj_c];
+                }
+            } else {
+                // Two bucket layers are present
+                status = 300;
+
+                // Checking which layer is lower
+                if (
+                    sim_out->body_[0][ii_c][jj_c] < sim_out->body_[2][ii_c][jj_c]
+                ) {
+                    // First bucket layer is lower
+                    bucket_bot = sim_out->body_[0][ii_c][jj_c];
+
+                    if (
+                        (sim_out->body_soil_[2][ii_c][jj_c] != 0.0) ||
+                        (sim_out->body_soil_[3][ii_c][jj_c] != 0.0)
+                    ) {
+                        // Bucket soil is present
+                        status += 10;
+                        column_top = sim_out->body_soil_[3][ii_c][jj_c];
+                    } else {
+                        // Bucket soil is not present
+                        status += 20;
+                        column_top = sim_out->body_[3][ii_c][jj_c];
+                    }
+                } else {
+                    // Second bucket layer is lower
+                    bucket_bot = sim_out->body_[2][ii_c][jj_c];
+
+                    if (
+                        (sim_out->body_soil_[0][ii_c][jj_c] != 0.0) ||
+                        (sim_out->body_soil_[1][ii_c][jj_c] != 0.0)
+                    ) {
+                        // Bucket soil is present
+                        status += 30;
+                        column_top = sim_out->body_soil_[1][ii_c][jj_c];
+                    } else {
+                        // Bucket soil is not present
+                        status += 40;
+                        column_top = sim_out->body_[1][ii_c][jj_c];
+                    }
+                }
+            }
+
+            if (sim_out->terrain_[ii_c][jj_c] + tol < bucket_bot) {
+                // Space under the bucket
+                return status + 1;
+            }
+
+            if (column_top + tol < h_min) {
+                // Column is low enough
+                return status + 2;
+            }
+        } else {
+            // No bucket
+            return 400;
+        }
+    }
+
     return 0;
 }
 
