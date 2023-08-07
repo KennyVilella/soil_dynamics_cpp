5def0d0c402d9540a449f4a9197f258ba7220574
Kenny Vilella <kennyvilella@gmail.com>
2023-08-07 09:17:43 +0900

    [added] unit tests for the LocateUnstableTerrainCell function

--- test/unit_tests/test_relax.cpp
+++ test/unit_tests/test_relax.cpp
@@ -7,6 +7,42 @@ Copyright, 2023, Vilella Kenny.
 #include "src/relax.cpp"
 
 TEST(UnitTestRelax, LocateUnstableTerrainCell) {
+    // Setting up the environment
+    soil_simulator::Grid grid(1.0, 1.0, 1.0, 0.1, 0.1);
+    soil_simulator::SimOut *sim_out = new soil_simulator::SimOut(grid);
+    sim_out->impact_area_[0][0] = 2;
+    sim_out->impact_area_[0][1] = 17;
+    sim_out->impact_area_[1][0] = 2;
+    sim_out->impact_area_[1][1] = 17;
+    sim_out->terrain_[2][2] = -0.1;
+    sim_out->terrain_[5][2] = -0.2;
+    sim_out->terrain_[11][13] = -0.2;
+    sim_out->terrain_[5][13] = 0.2;
+    sim_out->terrain_[7][13] = 0.1;
+    sim_out->terrain_[15][5] = -0.4;
+    sim_out->terrain_[15][6] = -0.2;
+
+    // -- Testing that all unstable cells are properly located --
+    auto unstable_cells = soil_simulator::LocateUnstableTerrainCell(
+        sim_out, 0.1, 1e-5);
+    EXPECT_TRUE((unstable_cells[0] == std::vector<int> {4, 2}));
+    EXPECT_TRUE((unstable_cells[1] == std::vector<int> {5, 3}));
+    EXPECT_TRUE((unstable_cells[2] == std::vector<int> {5, 13}));
+    EXPECT_TRUE((unstable_cells[3] == std::vector<int> {6, 2}));
+    EXPECT_TRUE((unstable_cells[4] == std::vector<int> {10, 13}));
+    EXPECT_TRUE((unstable_cells[5] == std::vector<int> {11, 12}));
+    EXPECT_TRUE((unstable_cells[6] == std::vector<int> {11, 14}));
+    EXPECT_TRUE((unstable_cells[7] == std::vector<int> {12, 13}));
+    EXPECT_TRUE((unstable_cells[8] == std::vector<int> {14, 5}));
+    EXPECT_TRUE((unstable_cells[9] == std::vector<int> {14, 6}));
+    EXPECT_TRUE((unstable_cells[10] == std::vector<int> {15, 4}));
+    EXPECT_TRUE((unstable_cells[11] == std::vector<int> {15, 6}));
+    EXPECT_TRUE((unstable_cells[12] == std::vector<int> {15, 7}));
+    EXPECT_TRUE((unstable_cells[13] == std::vector<int> {16, 5}));
+    EXPECT_TRUE((unstable_cells[14] == std::vector<int> {16, 6}));
+    EXPECT_EQ(unstable_cells.size(), 15);
+
+    delete sim_out;
 }
 
 TEST(UnitTestRelax, CheckUnstableTerrainCell) {
