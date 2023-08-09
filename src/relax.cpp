c166a7047c154c16358975e5b78100786a21129c
Kenny Vilella <kennyvilella@gmail.com>
2023-08-09 13:27:09 +0900

    [corrected] removed extra rng definition

--- src/relax.cpp
+++ src/relax.cpp
@@ -36,7 +36,6 @@ void soil_simulator::RelaxTerrain(
     // Randomizing unstable cells to reduce asymmetry
     // random_suffle is not used because it is machine dependent,
     // which makes unit testing difficult
-    std::mt19937 rng;
     for (int aa = unstable_cells.size() - 1; aa > 0; aa--) {
         std::uniform_int_distribution<int> dist(0, aa);
         int bb = dist(rng);
@@ -67,7 +66,6 @@ void soil_simulator::RelaxTerrain(
         // Randomizing direction to avoid asymmetry
         // random_suffle is not used because it is machine dependent,
         // which makes unit testing difficult
-        std::mt19937 rng;
         for (int aa = directions.size() - 1; aa > 0; aa--) {
             std::uniform_int_distribution<int> dist(0, aa);
             int bb = dist(rng);
