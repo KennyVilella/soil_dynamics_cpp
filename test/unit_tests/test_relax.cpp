114c693ca73a2302731015f74fd6a1e8d296aa32
Kenny Vilella <kennyvilella@gmail.com>
2023-08-10 11:36:52 +0900

    [corrected] delete pointer in unit tests

--- test/unit_tests/test_relax.cpp
+++ test/unit_tests/test_relax.cpp
@@ -3983,6 +3983,7 @@ TEST(UnitTestRelax, RelaxUnstableBodyCell) {
     body_soil_pos->erase(body_soil_pos->begin(), body_soil_pos->end());
 
     delete sim_out;
+    delete body_soil_pos;
 }
 
 TEST(UnitTestRelax, RelaxBodySoil) {
