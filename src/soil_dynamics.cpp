71376b360ad74aa1255e4155ef8acd01666b08eb
Kenny Vilella <kennyvilella@gmail.com>
2023-08-10 17:16:01 +0900

    [added] preliminay impllementation for the step function

--- src/soil_dynamics.cpp
+++ src/soil_dynamics.cpp
@@ -5,13 +5,34 @@ Copyright, 2023, Vilella Kenny.
 */
 #include <iostream>
 #include <random>
+#include <vector>
 #include "src/soil_dynamics.hpp"
 #include "src/types.hpp"
+#include "src/bucket_pos.hpp"
+#include "src/body_soil.hpp"
+#include "src/intersecting_cells.hpp"
+#include "src/relax.hpp"
+#include "src/utils.hpp"
 
 // Defining RNG
 std::mt19937 soil_simulator::rng;
 
-void soil_simulator::SoilDynamics::step() {
+void soil_simulator::SoilDynamics::step(
+    SimOut* sim_out, std::vector<float> pos, std::vector<float> ori,
+    Grid grid, Bucket* bucket, SimParam sim_param, float tol
+) {
+    // Updating bucket position
+    soil_simulator::CalcBucketPos(
+        sim_out, pos, ori, grid, bucket, sim_param, tol);
+
+    // Updating position of soil resting on the bucket
+    soil_simulator::UpdateBodySoil(sim_out, pos, ori, grid, bucket, tol);
+
+    // Moving intersecting soil cells
+    soil_simulator::MoveIntersectingCells(sim_out, tol);
+
+    // Assuming that the terrain is not at equilibrium
+    sim_out->equilibrium_ = false;
 }
 
 void soil_simulator::SoilDynamics::check() {
