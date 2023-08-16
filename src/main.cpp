1f1fd9a9a820cabbc2df1ee12245337ecedc0fc2
Kenny Vilella <kennyvilella@gmail.com>
2023-08-16 10:07:33 +0900

    [added] a separate minimalist main function

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+/*
+This file implements the main function.
+
+Copyright, 2023, Vilella Kenny.
+*/
+#include <random>
+#include "src/soil_dynamics.hpp"
+#include "src/types.hpp"
+#include "src/bucket_pos.hpp"
+#include "src/body_soil.hpp"
+#include "src/intersecting_cells.hpp"
+#include "src/relax.hpp"
+#include "src/utils.hpp"
+
+// Defining RNG
+std::mt19937 soil_simulator::rng;
+
+int main() {
+    return 0;
+}
