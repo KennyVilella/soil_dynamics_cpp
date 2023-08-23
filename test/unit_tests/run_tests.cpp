c88f0415a09096d846c369e2cea9249001fa8671
Kenny Vilella <kennyvilella@gmail.com>
2023-08-23 09:48:02 +0900

    [changed] log are sent to stderr instead of logfiles for unit test

--- test/unit_tests/run_tests.cpp
+++ test/unit_tests/run_tests.cpp
@@ -4,6 +4,7 @@ This file runs all unit tests.
 Copyright, 2023, Vilella Kenny.
 */
 #include <random>
+#include <glog/logging.h>
 #include "gtest/gtest.h"
 #include "src/soil_dynamics.hpp"
 
@@ -11,6 +12,12 @@ Copyright, 2023, Vilella Kenny.
 std::mt19937 soil_simulator::rng;
 
 int main(int argc, char **argv) {
+    // Writting outputs to stderr instead of logfiles
+    FLAGS_logtostderr = 1;
+
+    // Disabling Warning messages
+    FLAGS_minloglevel = 2;
+
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
