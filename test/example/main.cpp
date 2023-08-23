01011320576a0371f41773e982aa1c31a7cd8929
Kenny Vilella <kennyvilella@gmail.com>
2023-08-23 10:04:53 +0900

    [changed] log are sent to stderr instead of logfiles for the example script

--- test/example/main.cpp
+++ test/example/main.cpp
@@ -4,6 +4,7 @@ This file implements the main function.
 Copyright, 2023, Vilella Kenny.
 */
 #include <random>
+#include <glog/logging.h>
 #include "src/soil_dynamics.hpp"
 #include "test/example/soil_evolution.hpp"
 
@@ -11,6 +12,9 @@ Copyright, 2023, Vilella Kenny.
 std::mt19937 soil_simulator::rng;
 
 int main() {
+    // Writting outputs to stderr instead of logfiles
+    FLAGS_logtostderr = 1;
+
     // Launching a simulation
     soil_simulator::SoilEvolution(true, false, true, true, false);
 
