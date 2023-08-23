027df6a096d827593525ea47804b26706e7b6cb8
Kenny Vilella <kennyvilella@gmail.com>
2023-08-23 10:16:02 +0900

    [added] glog initialization for example script

--- test/example/main.cpp
+++ test/example/main.cpp
@@ -11,7 +11,10 @@ Copyright, 2023, Vilella Kenny.
 // Defining RNG
 std::mt19937 soil_simulator::rng;
 
-int main() {
+int main(int argc, char* argv[]) {
+    // Initialize Google’s logging library.
+    google::InitGoogleLogging(argv[0]);
+
     // Writting outputs to stderr instead of logfiles
     FLAGS_logtostderr = 1;
 
