d979d92285a513aa2ea5a2afea6ae3adc51a86d5
Kenny Vilella <kennyvilella@gmail.com>
2023-09-12 13:48:28 +0900

    [updated] function in intersecting_cells.cpp following new body_soil_pos_

--- soil_simulator/intersecting_cells.hpp
+++ soil_simulator/intersecting_cells.hpp
@@ -19,14 +19,16 @@ extern std::mt19937 rng;
 ///
 /// \param sim_out: Class that stores simulation outputs.
 /// \param tol: Small number used to handle numerical approximation errors.
-void MoveIntersectingCells(SimOut* sim_out, float tol);
+void MoveIntersectingCells(
+    SimOut* sim_out, Grid grid, Bucket* bucket, float tol);
 
 /// \brief This function moves the soil cells resting on the bucket that
 ///        intersect with another bucket layer.
 ///
 /// \param sim_out: Class that stores simulation outputs.
 /// \param tol: Small number used to handle numerical approximation errors.
-void MoveIntersectingBodySoil(SimOut* sim_out, float tol);
+void MoveIntersectingBodySoil(
+    SimOut* sim_out, Grid grid, Bucket* bucket, float tol);
 
 /// \brief This function moves the soil cells in the `terrain_` that intersect
 ///        with a bucket.
@@ -57,7 +59,8 @@ void MoveIntersectingBody(SimOut* sim_out, float tol);
 ///         whether a bucket wall is blocking the movement.
 std::tuple<int, int, int, float, bool> MoveBodySoil(
     SimOut* sim_out, int ind_p, int ii_p, int jj_p, float max_h, int ii_n,
-    int jj_n, float h_soil, bool wall_presence, float tol);
+    int jj_n, float h_soil, bool wall_presence, Grid grid, Bucket* bucket,
+    float tol);
 
 /// \brief This function identifies all the soil cells in the `terrain_` that
 ///        intersect with the bucket.
