f9c0e7d6c7296439082472f963d859e1a070ced4
Kenny Vilella <kennyvilella@gmail.com>
2023-09-15 14:37:51 +0900

    [corrected] benchmark tests

--- test/benchmarks/benchmark_relax.cpp
+++ test/benchmarks/benchmark_relax.cpp
@@ -6,11 +6,18 @@ Copyright, 2023, Vilella Kenny.
 #include <benchmark/benchmark.h>
 #include <random>
 #include "soil_simulator/relax.hpp"
+#include "soil_simulator/utils.hpp"
 
 // -- RelaxTerrain --
 static void BM_RelaxTerrain(benchmark::State& state) {
     // Defining inputs
     soil_simulator::Grid grid(4.0, 4.0, 3.0, 0.05, 0.01);
+    std::vector<float> o_pos = {0.0, 0.0, 0.0};
+    std::vector<float> j_pos = {0.0, 0.0, 0.0};
+    std::vector<float> b_pos = {0.0, 0.0, -0.5};
+    std::vector<float> t_pos = {0.7, 0.0, -0.5};
+    soil_simulator::Bucket *bucket = new soil_simulator::Bucket(
+        o_pos, j_pos, b_pos, t_pos, 0.5);
     soil_simulator::SimParam sim_param(0.85, 3, 4);
     soil_simulator::SimOut *sim_out = new soil_simulator::SimOut(grid);
     for (auto ii = 49; ii < 65; ii++)
@@ -32,7 +39,8 @@ static void BM_RelaxTerrain(benchmark::State& state) {
     sim_out->impact_area_[1][1] = 68;
 
     for (auto _ : state)
-        soil_simulator::RelaxTerrain(sim_out, grid, sim_param, 1.e-5);
+        soil_simulator::RelaxTerrain(sim_out, grid, bucket, sim_param, 1.e-5);
+    delete bucket;
     delete sim_out;
 }
 BENCHMARK(BM_RelaxTerrain)->Unit(benchmark::kMicrosecond);
@@ -41,6 +49,12 @@ BENCHMARK(BM_RelaxTerrain)->Unit(benchmark::kMicrosecond);
 static void BM_RelaxBodySoil(benchmark::State& state) {
     // Defining inputs
     soil_simulator::Grid grid(4.0, 4.0, 3.0, 0.05, 0.01);
+    std::vector<float> o_pos = {0.0, 0.0, 0.0};
+    std::vector<float> j_pos = {0.0, 0.0, 0.0};
+    std::vector<float> b_pos = {0.0, 0.0, -0.5};
+    std::vector<float> t_pos = {0.7, 0.0, -0.5};
+    soil_simulator::Bucket *bucket = new soil_simulator::Bucket(
+        o_pos, j_pos, b_pos, t_pos, 0.5);
     soil_simulator::SimParam sim_param(0.85, 3, 4);
     soil_simulator::SimOut *sim_out = new soil_simulator::SimOut(grid);
     for (auto ii = 49; ii < 65; ii++)
@@ -53,14 +67,20 @@ static void BM_RelaxBodySoil(benchmark::State& state) {
             sim_out->body_[1][ii][jj] = 0.1;
             sim_out->body_soil_[0][ii][jj] = 0.1;
             sim_out->body_soil_[1][ii][jj] = 0.4;
-            sim_out->body_soil_pos_.push_back(std::vector<int> {0, ii, jj});
+            auto pos = soil_simulator::CalcBucketFramePos(
+                ii, jj, 0.1, grid, bucket);
+            sim_out->body_soil_pos_.push_back(soil_simulator::body_soil
+                 {0, ii, jj, pos[0], pos[1], pos[2], 0.3});
         }
     for (auto ii = 49; ii < 65; ii++)
         for (auto jj = 60; jj < 65; jj++) {
             sim_out->body_[1][ii][jj] = 0.4;
             sim_out->body_soil_[0][ii][jj] = 0.4;
             sim_out->body_soil_[1][ii][jj] = 0.7;
-            sim_out->body_soil_pos_.push_back(std::vector<int> {0, ii, jj});
+            auto pos = soil_simulator::CalcBucketFramePos(
+                ii, jj, 0.1, grid, bucket);
+            sim_out->body_soil_pos_.push_back(soil_simulator::body_soil
+                {0, ii, jj, pos[0], pos[1], pos[2], 0.3});
         }
     sim_out->impact_area_[0][0] = 44;
     sim_out->impact_area_[1][0] = 45;
@@ -68,7 +88,8 @@ static void BM_RelaxBodySoil(benchmark::State& state) {
     sim_out->impact_area_[1][1] = 68;
 
     for (auto _ : state)
-        soil_simulator::RelaxBodySoil(sim_out, grid, sim_param, 1.e-5);
+        soil_simulator::RelaxBodySoil(sim_out, grid, bucket, sim_param, 1.e-5);
+    delete bucket;
     delete sim_out;
 }
 BENCHMARK(BM_RelaxBodySoil)->Unit(benchmark::kMicrosecond);
@@ -111,6 +132,12 @@ BENCHMARK(BM_CheckUnstableTerrainCell);
 static void BM_RelaxUnstableTerrainCell(benchmark::State& state) {
     // Defining inputs
     soil_simulator::Grid grid(4.0, 4.0, 3.0, 0.05, 0.01);
+    std::vector<float> o_pos = {0.0, 0.0, 0.0};
+    std::vector<float> j_pos = {0.0, 0.0, 0.0};
+    std::vector<float> b_pos = {0.0, 0.0, -0.5};
+    std::vector<float> t_pos = {0.7, 0.0, -0.5};
+    soil_simulator::Bucket *bucket = new soil_simulator::Bucket(
+        o_pos, j_pos, b_pos, t_pos, 0.5);
     soil_simulator::SimParam sim_param(0.85, 3, 4);
     soil_simulator::SimOut *sim_out = new soil_simulator::SimOut(grid);
     sim_out->terrain_[50][55] = 0.4;
@@ -119,7 +146,8 @@ static void BM_RelaxUnstableTerrainCell(benchmark::State& state) {
 
     for (auto _ : state)
         soil_simulator::RelaxUnstableTerrainCell(
-            sim_out, 142, 0.1, 50, 55, 49, 55, grid, 1.e-5);
+            sim_out, 142, 0.1, 50, 55, 49, 55, grid, bucket, 1.e-5);
+    delete bucket;
     delete sim_out;
 }
 BENCHMARK(BM_RelaxUnstableTerrainCell);
@@ -128,6 +156,12 @@ BENCHMARK(BM_RelaxUnstableTerrainCell);
 static void BM_CheckUnstableBodyCell(benchmark::State& state) {
     // Defining inputs
     soil_simulator::Grid grid(4.0, 4.0, 3.0, 0.05, 0.01);
+    std::vector<float> o_pos = {0.0, 0.0, 0.0};
+    std::vector<float> j_pos = {0.0, 0.0, 0.0};
+    std::vector<float> b_pos = {0.0, 0.0, -0.5};
+    std::vector<float> t_pos = {0.7, 0.0, -0.5};
+    soil_simulator::Bucket *bucket = new soil_simulator::Bucket(
+        o_pos, j_pos, b_pos, t_pos, 0.5);
     soil_simulator::SimParam sim_param(0.85, 3, 4);
     soil_simulator::SimOut *sim_out = new soil_simulator::SimOut(grid);
     sim_out->terrain_[50][61] = 0.4;
@@ -140,13 +174,17 @@ static void BM_CheckUnstableBodyCell(benchmark::State& state) {
     sim_out->body_[1][50][60] = 0.1;
     sim_out->body_soil_[0][50][60] = 0.1;
     sim_out->body_soil_[1][50][60] = 0.4;
-    sim_out->body_soil_pos_.push_back(std::vector<int> {0, 50, 61});
-    sim_out->body_soil_pos_.push_back(std::vector<int> {0, 50, 60});
-
+    auto pos = soil_simulator::CalcBucketFramePos(50, 61, 0.4, grid, bucket);
+    sim_out->body_soil_pos_.push_back(
+        soil_simulator::body_soil {0, 50, 61, pos[0], pos[1], pos[2], 0.3});
+    pos = soil_simulator::CalcBucketFramePos(50, 60, 0.1, grid, bucket);
+    sim_out->body_soil_pos_.push_back(
+        soil_simulator::body_soil {0, 50, 60, pos[0], pos[1], pos[2], 0.3});
     for (auto _ : state)
         soil_simulator::CheckUnstableBodyCell(
             sim_out, 50, 61, 0, 50, 60, 0.1, 1.e-5);
     delete sim_out;
+    delete bucket;
 }
 BENCHMARK(BM_CheckUnstableBodyCell);
 
@@ -155,6 +193,12 @@ static void BM_RelaxUnstableBodyCell(benchmark::State& state) {
     // Defining inputs
     soil_simulator::Grid grid(4.0, 4.0, 3.0, 0.05, 0.01);
     soil_simulator::SimParam sim_param(0.85, 3, 4);
+    std::vector<float> o_pos = {0.0, 0.0, 0.0};
+    std::vector<float> j_pos = {0.0, 0.0, 0.0};
+    std::vector<float> b_pos = {0.0, 0.0, -0.5};
+    std::vector<float> t_pos = {0.7, 0.0, -0.5};
+    soil_simulator::Bucket *bucket = new soil_simulator::Bucket(
+        o_pos, j_pos, b_pos, t_pos, 0.5);
     soil_simulator::SimOut *sim_out = new soil_simulator::SimOut(grid);
     sim_out->terrain_[50][61] = 0.4;
     sim_out->body_[0][50][61] = 0.0;
@@ -166,14 +210,20 @@ static void BM_RelaxUnstableBodyCell(benchmark::State& state) {
     sim_out->body_[1][50][60] = 0.1;
     sim_out->body_soil_[0][50][60] = 0.1;
     sim_out->body_soil_[1][50][60] = 0.4;
-    sim_out->body_soil_pos_.push_back(std::vector<int> {0, 50, 61});
-    sim_out->body_soil_pos_.push_back(std::vector<int> {0, 50, 60});
-    std::vector<std::vector<int>> *body_soil_pos = (
-        new std::vector<std::vector<int>>);
+    auto pos = soil_simulator::CalcBucketFramePos(50, 61, 0.4, grid, bucket);
+    sim_out->body_soil_pos_.push_back(
+        soil_simulator::body_soil {0, 50, 61, pos[0], pos[1], pos[2], 0.3});
+    pos = soil_simulator::CalcBucketFramePos(50, 60, 0.1, grid, bucket);
+    sim_out->body_soil_pos_.push_back(
+        soil_simulator::body_soil {0, 50, 60, pos[0], pos[1], pos[2], 0.3});
+    std::vector<soil_simulator::body_soil> *body_soil_pos = (
+        new std::vector<soil_simulator::body_soil>);
 
     for (auto _ : state)
         soil_simulator::RelaxUnstableBodyCell(
-            sim_out, 13, body_soil_pos, 0.1, 50, 61, 0, 50, 60, grid, 1.e-5);
+            sim_out, 13, body_soil_pos, 0.1, 0, 50, 61, 0, 50, 60, grid, bucket,
+            1.e-5);
+    delete bucket;
     delete sim_out;
     delete body_soil_pos;
 }
