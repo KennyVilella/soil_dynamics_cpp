245243d0dae91b136d51e7e48a7d6af78b6420e6
Kenny Vilella <kennyvilella@gmail.com>
2023-10-09 10:39:05 +0900

    [updated] test_types.cpp to include unit test ID

--- test/unit_tests/test_types.cpp
+++ test/unit_tests/test_types.cpp
@@ -10,9 +10,8 @@ Copyright, 2023, Vilella Kenny.
 #include "soil_simulator/types.hpp"
 
 TEST(UnitTestTypes, Grid) {
-    // -- Testing member of the Grid class --
+    // Test: TY-G-1
     soil_simulator::Grid grid(4.0, 4.0, 4.0, 0.05, 0.01);
-
     EXPECT_EQ(grid.half_length_x_, 80);
     EXPECT_EQ(grid.half_length_y_, 80);
     EXPECT_EQ(grid.half_length_z_, 400);
@@ -27,67 +26,78 @@ TEST(UnitTestTypes, Grid) {
     for (auto ii = 0 ; ii < grid.vect_z_.size() ; ii++)
         EXPECT_NEAR(grid.vect_z_[ii], -4.0 + (ii + 1) * 0.01, 1e-5);
 
-    // -- Testing that exceptions are properly sent --
+    // Test: TY-G-2
     EXPECT_THROW(
         soil_simulator::Grid grid(4.0, 4.0, 4.0, 0.05, 0.0),
-        std::invalid_argument);  // size_z = 0.0
+        std::invalid_argument);
     EXPECT_THROW(
         soil_simulator::Grid grid(4.0, 4.0, 4.0, 0.05, -0.01),
-        std::invalid_argument);  // size_z < 0.0
+        std::invalid_argument);
+
+    // Test: TY-G-3
     EXPECT_THROW(
         soil_simulator::Grid grid(4.0, 4.0, 4.0, 0.0, 0.01),
-        std::invalid_argument);  // size_xy = 0.0
+        std::invalid_argument);
     EXPECT_THROW(
         soil_simulator::Grid grid(4.0, 4.0, 4.0, -0.05, 0.01),
-        std::invalid_argument);  // size_xy < 0.0
+        std::invalid_argument);
+
+    // Test: TY-G-4
     EXPECT_THROW(
         soil_simulator::Grid grid(0.0, 4.0, 4.0, 0.05, 0.01),
-        std::invalid_argument);  // grid_size_x = 0.0
+        std::invalid_argument);
     EXPECT_THROW(
         soil_simulator::Grid grid(-1.0, 4.0, 4.0, 0.05, 0.01),
-        std::invalid_argument);  // grid_size_x < 0.0
+        std::invalid_argument);
+
+    // Test: TY-G-5
     EXPECT_THROW(
         soil_simulator::Grid grid(4.0, 0.0, 4.0, 0.05, 0.01),
-        std::invalid_argument);  // grid_size_y = 0.0
+        std::invalid_argument);
     EXPECT_THROW(
         soil_simulator::Grid grid(4.0, -2.0, 4.0, 0.05, 0.01),
-        std::invalid_argument);  // grid_size_y < 0.0
+        std::invalid_argument);
+
+    // Test: TY-G-6
     EXPECT_THROW(
         soil_simulator::Grid grid(4.0, 4.0, 0.0, 0.05, 0.01),
-        std::invalid_argument);  // grid_size_z = 0.0
+        std::invalid_argument);
     EXPECT_THROW(
         soil_simulator::Grid grid(4.0, 4.0, -4.0, 0.05, 0.01),
-        std::invalid_argument);  // grid_size_z < 0.0
+        std::invalid_argument); 
+
+    // Test: TY-G-7
     EXPECT_THROW(
         soil_simulator::Grid grid(4.0, 4.0, 4.0, 0.05, 0.06),
-        std::invalid_argument);  // size_z > size_xy
-    // size_z = size_xy
+        std::invalid_argument);
     EXPECT_NO_THROW(soil_simulator::Grid grid(4.0, 4.0, 4.0, 0.05, 0.05));
+
+    // Test: TY-G-8
     EXPECT_THROW(
         soil_simulator::Grid grid(0.04, 4.0, 4.0, 0.05, 0.05),
-        std::invalid_argument);  // grid_size_x < size_xy
-    // grid_size_x = size_xy
+        std::invalid_argument);
     EXPECT_NO_THROW(soil_simulator::Grid grid(0.05, 4.0, 4.0, 0.05, 0.05));
+
+    // Test: TY-G-9
     EXPECT_THROW(
         soil_simulator::Grid grid(4.0, 0.01, 4.0, 0.05, 0.05),
-        std::invalid_argument);  // grid_size_y < size_xy
-    // grid_size_y = size_xy
+        std::invalid_argument);
     EXPECT_NO_THROW(soil_simulator::Grid grid(4.0, 0.05, 4.0, 0.05, 0.05));
+
+    // Test: TY-G-10
     EXPECT_THROW(
         soil_simulator::Grid grid(4.0, 4.0, 0.01, 0.05, 0.03),
-        std::invalid_argument);  // grid_size_z < size_z
-    // grid_size_z = size_z
+        std::invalid_argument);
     EXPECT_NO_THROW(soil_simulator::Grid grid(4.0, 4.0, 0.03, 0.05, 0.03));
 }
 
 TEST(UnitTestTypes, Bucket) {
-    // -- Testing member of the Bucket class --
+    // Test: TY-B-1
     std::vector<float> o_pos = {0.0, 0.1, 0.0};
     std::vector<float> j_pos = {0.0, 0.5, 0.0};
     std::vector<float> b_pos = {0.0, 0.5, 0.5};
     std::vector<float> t_pos = {0.5, 0.5, 0.0};
     soil_simulator::Bucket bucket(o_pos, j_pos, b_pos, t_pos, 0.5);
-
     EXPECT_NEAR(bucket.j_pos_init_[0], 0.0, 1e-8);
     EXPECT_NEAR(bucket.j_pos_init_[1], 0.4, 1e-8);
     EXPECT_NEAR(bucket.j_pos_init_[2], 0.0, 1e-8);
@@ -103,69 +113,86 @@ TEST(UnitTestTypes, Bucket) {
     for (auto ii = 0 ; ii < 4 ; ii++)
         EXPECT_EQ(bucket.ori_[ii], 0.0);
 
-    // -- Testing that exceptions are properly sent --
+    // Setting up some dummy vect
     std::vector<float> vect_1 = {0.0};
     std::vector<float> vect_2 = {0.0, 0.1};
     std::vector<float> vect_4 = {0.0, 0.1, 0.0, 0.3};
+
+    // Test: TY-B-2
     EXPECT_THROW(
         soil_simulator::Bucket bucket(vect_1, j_pos, b_pos, t_pos, 0.5),
-        std::invalid_argument);  // o_pos.size() != 3
+        std::invalid_argument);
+
+    // Test: TY-B-3
     EXPECT_THROW(
         soil_simulator::Bucket bucket(o_pos, vect_2, b_pos, t_pos, 0.5),
-        std::invalid_argument);  // j_pos.size() != 3
+        std::invalid_argument);
+
+    // Test: TY-B-4
     EXPECT_THROW(
         soil_simulator::Bucket bucket(o_pos, j_pos, vect_4, t_pos, 0.5),
-        std::invalid_argument);  // b_pos.size() != 3
+        std::invalid_argument);
+
+    // Test: TY-B-5
     EXPECT_THROW(
         soil_simulator::Bucket bucket(o_pos, j_pos, b_pos, vect_2, 0.5),
-        std::invalid_argument);  // t_pos.size() != 3
+        std::invalid_argument);
+
+    // Test: TY-B-6
     EXPECT_THROW(
         soil_simulator::Bucket bucket(o_pos, b_pos, b_pos, t_pos, 0.5),
-        std::invalid_argument);  // j_pos == b_pos
+        std::invalid_argument);
+
+    // Test: TY-B-7
     EXPECT_THROW(
         soil_simulator::Bucket bucket(o_pos, t_pos, b_pos, t_pos, 0.5),
-        std::invalid_argument);  // j_pos == t_pos
+        std::invalid_argument);
+
+    // Test: TY-B-8
     EXPECT_THROW(
         soil_simulator::Bucket bucket(o_pos, j_pos, b_pos, b_pos, 0.5),
-        std::invalid_argument);  // b_pos == t_pos
+        std::invalid_argument);
+
+    // Test: TY-B-9
     EXPECT_THROW(
         soil_simulator::Bucket bucket(o_pos, j_pos, b_pos, t_pos, 0.0),
-        std::invalid_argument);  // bucket_width = 0.0
+        std::invalid_argument);
     EXPECT_THROW(
         soil_simulator::Bucket bucket(o_pos, j_pos, b_pos, t_pos, -0.5),
-        std::invalid_argument);  // bucket_width < 0.0
+        std::invalid_argument);
 }
 
 TEST(UnitTestTypes, SimParam) {
-    // -- Testing member of the SimParam class --
+    // Test: TY-SP-1
     soil_simulator::SimParam sim_param(0.85, 5, 4);
-
     EXPECT_NEAR(sim_param.repose_angle_, 0.85, 1e-7);
     EXPECT_EQ(sim_param.max_iterations_, 5);
     EXPECT_EQ(sim_param.cell_buffer_, 4);
 
-    // -- Testing that exceptions are properly sent --
+    // Test: TY-SP-2
     EXPECT_THROW(
         soil_simulator::SimParam sim_param(3.14, 5, 4),
-        std::invalid_argument);  // repose_angle > pi/2
+        std::invalid_argument);
     EXPECT_THROW(
         soil_simulator::SimParam sim_param(-0.85, 5, 4),
-        std::invalid_argument);  // repose_angle < 0.0
-    // max_iterations = 0.0
+        std::invalid_argument);
+
+    // Test: TY-SP-3
     EXPECT_NO_THROW(soil_simulator::SimParam sim_param(0.85, 0.0, 4));
     EXPECT_THROW(
         soil_simulator::SimParam sim_param(0.85, -5, 4),
-        std::invalid_argument);  // max_iterations < 0.0
+        std::invalid_argument); 
+
+    // Test: TY-SP-4
     EXPECT_THROW(
         soil_simulator::SimParam sim_param(0.85, 5, 1),
-        std::invalid_argument);  // cell_buffer < 2.0
+        std::invalid_argument);
 }
 
 TEST(UnitTestTypes, SimOut) {
-    // -- Testing member of the SimOut class --
+    // Test: TY-SO-1
     soil_simulator::Grid grid(2.0, 2.0, 2.0, 1.0, 0.01);
     soil_simulator::SimOut sim_out(grid);
-
     EXPECT_EQ(sim_out.equilibrium_, false);
     for (auto ii = 0 ; ii < 5 ; ii++)
         for (auto jj = 0 ; jj < 5 ; jj++)
