7a7b5c07a349858a0f64af524eb7212aa9c01359
Kenny Vilella <kennyvilella@gmail.com>
2023-10-30 10:50:11 +0900

    [changed] split down unit test for CheckUnstableTerrainCell into separate tests

--- test/unit_tests/test_relax.cpp
+++ test/unit_tests/test_relax.cpp
@@ -29,35 +29,79 @@ TEST(UnitTestRelax, LocateUnstableTerrainCell) {
     sim_out->impact_area_[0][1] = 17;
     sim_out->impact_area_[1][0] = 2;
     sim_out->impact_area_[1][1] = 17;
-    sim_out->terrain_[2][2] = -0.1;
-    sim_out->terrain_[5][2] = -0.2;
-    sim_out->terrain_[11][13] = -0.2;
-    sim_out->terrain_[5][13] = 0.2;
-    sim_out->terrain_[7][13] = 0.1;
-    sim_out->terrain_[15][5] = -0.4;
-    sim_out->terrain_[15][6] = -0.2;
 
     // Declaring variables
     std::vector<std::vector<int>> unstable_cells;
 
-    // -- Testing that all unstable cells are properly located --
+    // Test: RE-LUT-1
+    unstable_cells = LocateUnstableTerrainCell(sim_out, 0.1, 1e-5);
+    EXPECT_EQ(unstable_cells.size(), 0);
+
+    // Test: RE-LUT-2
+    sim_out->terrain_[7][13] = 0.1;
+    unstable_cells = LocateUnstableTerrainCell(sim_out, 0.1, 1e-5);
+    EXPECT_EQ(unstable_cells.size(), 0);
+    unstable_cells = LocateUnstableTerrainCell(sim_out, 0.0, 1e-5);
+    EXPECT_TRUE((unstable_cells[0] == std::vector<int> {7, 13}));
+    EXPECT_EQ(unstable_cells.size(), 1);
+    sim_out->terrain_[7][13] = 0.0;
+
+    // Test: RE-LUT-3
+    sim_out->terrain_[5][13] = 0.2;
+    unstable_cells = LocateUnstableTerrainCell(sim_out, 0.1, 1e-5);
+    EXPECT_TRUE((unstable_cells[0] == std::vector<int> {5, 13}));
+    EXPECT_EQ(unstable_cells.size(), 1);
+    unstable_cells = LocateUnstableTerrainCell(sim_out, 0.2, 1e-5);
+    EXPECT_EQ(unstable_cells.size(), 0);
+    sim_out->terrain_[5][13] = 0.0;
+
+    // Test: RE-LUT-4
+    sim_out->terrain_[9][9] = -0.1;
+    unstable_cells = LocateUnstableTerrainCell(sim_out, 0.1, 1e-5);
+    EXPECT_EQ(unstable_cells.size(), 0);
+    unstable_cells = LocateUnstableTerrainCell(sim_out, 0.0, 1e-5);
+    EXPECT_TRUE((unstable_cells[0] == std::vector<int> {8, 9}));
+    EXPECT_TRUE((unstable_cells[1] == std::vector<int> {9, 8}));
+    EXPECT_TRUE((unstable_cells[2] == std::vector<int> {9, 10}));
+    EXPECT_TRUE((unstable_cells[3] == std::vector<int> {10, 9}));
+    EXPECT_EQ(unstable_cells.size(), 4);
+    sim_out->terrain_[9][9] = 0.0;
+
+    // Test: RE-LUT-5
+    sim_out->terrain_[11][13] = -0.2;
+    unstable_cells = LocateUnstableTerrainCell(sim_out, 0.1, 1e-5);
+    EXPECT_TRUE((unstable_cells[0] == std::vector<int> {10, 13}));
+    EXPECT_TRUE((unstable_cells[1] == std::vector<int> {11, 12}));
+    EXPECT_TRUE((unstable_cells[2] == std::vector<int> {11, 14}));
+    EXPECT_TRUE((unstable_cells[3] == std::vector<int> {12, 13}));
+    EXPECT_EQ(unstable_cells.size(), 4);
+    unstable_cells = LocateUnstableTerrainCell(sim_out, 0.2, 1e-5);
+    EXPECT_EQ(unstable_cells.size(), 0);
+    sim_out->terrain_[11][13] = 0.0;
+
+    // Test: RE-LUT-6
+    sim_out->terrain_[15][5] = -0.4;
+    sim_out->terrain_[15][6] = -0.2;
+    unstable_cells = LocateUnstableTerrainCell(sim_out, 0.1, 1e-5);
+    EXPECT_TRUE((unstable_cells[0] == std::vector<int> {14, 5}));
+    EXPECT_TRUE((unstable_cells[1] == std::vector<int> {14, 6}));
+    EXPECT_TRUE((unstable_cells[2] == std::vector<int> {15, 4}));
+    EXPECT_TRUE((unstable_cells[3] == std::vector<int> {15, 6}));
+    EXPECT_TRUE((unstable_cells[4] == std::vector<int> {15, 7}));
+    EXPECT_TRUE((unstable_cells[5] == std::vector<int> {16, 5}));
+    EXPECT_TRUE((unstable_cells[6] == std::vector<int> {16, 6}));
+    EXPECT_EQ(unstable_cells.size(), 7);
+    sim_out->terrain_[15][5] = 0.0;
+    sim_out->terrain_[15][6] = 0.0;
+
+    // Test: RE-LUT-7
+    sim_out->terrain_[5][2] = -0.2;
     unstable_cells = LocateUnstableTerrainCell(sim_out, 0.1, 1e-5);
     EXPECT_TRUE((unstable_cells[0] == std::vector<int> {4, 2}));
     EXPECT_TRUE((unstable_cells[1] == std::vector<int> {5, 3}));
-    EXPECT_TRUE((unstable_cells[2] == std::vector<int> {5, 13}));
-    EXPECT_TRUE((unstable_cells[3] == std::vector<int> {6, 2}));
-    EXPECT_TRUE((unstable_cells[4] == std::vector<int> {10, 13}));
-    EXPECT_TRUE((unstable_cells[5] == std::vector<int> {11, 12}));
-    EXPECT_TRUE((unstable_cells[6] == std::vector<int> {11, 14}));
-    EXPECT_TRUE((unstable_cells[7] == std::vector<int> {12, 13}));
-    EXPECT_TRUE((unstable_cells[8] == std::vector<int> {14, 5}));
-    EXPECT_TRUE((unstable_cells[9] == std::vector<int> {14, 6}));
-    EXPECT_TRUE((unstable_cells[10] == std::vector<int> {15, 4}));
-    EXPECT_TRUE((unstable_cells[11] == std::vector<int> {15, 6}));
-    EXPECT_TRUE((unstable_cells[12] == std::vector<int> {15, 7}));
-    EXPECT_TRUE((unstable_cells[13] == std::vector<int> {16, 5}));
-    EXPECT_TRUE((unstable_cells[14] == std::vector<int> {16, 6}));
-    EXPECT_EQ(unstable_cells.size(), 15);
+    EXPECT_TRUE((unstable_cells[2] == std::vector<int> {6, 2}));
+    EXPECT_EQ(unstable_cells.size(), 3);
+    sim_out->terrain_[5][2] = 0.0;
 
     delete sim_out;
 }
