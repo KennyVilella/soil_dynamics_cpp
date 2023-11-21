781e05c930da0840275dd9a668f068178fb3a030
Kenny Vilella <kennyvilella@gmail.com>
2023-11-21 11:56:01 +0900

    [changed] bucket -> body in intersectin_cells files

--- soil_simulator/intersecting_cells.hpp
+++ soil_simulator/intersecting_cells.hpp
@@ -15,38 +15,38 @@ namespace soil_simulator {
 extern std::mt19937 rng;
 
 /// \brief This function moves all soil cells in `terrain_` and in `body_soil_`
-///        that intersect with the bucket or with another soil cell.
+///        that intersect with the body or with another soil cell.
 ///
 /// \param sim_out: Class that stores simulation outputs.
 /// \param grid: Class that stores information related to the simulation grid.
-/// \param bucket: Class that stores information related to the bucket object.
+/// \param body: Class that stores information related to the body object.
 /// \param tol: Small number used to handle numerical approximation errors.
 void MoveIntersectingCells(
-    SimOut* sim_out, Grid grid, Bucket* bucket, float tol);
+    SimOut* sim_out, Grid grid, Body* body, float tol);
 
-/// \brief This function moves the soil cells resting on the bucket that
-///        intersect with another bucket layer.
+/// \brief This function moves the soil cells resting on the body that
+///        intersect with another body layer.
 ///
 /// \param sim_out: Class that stores simulation outputs.
 /// \param grid: Class that stores information related to the simulation grid.
-/// \param bucket: Class that stores information related to the bucket object.
+/// \param body: Class that stores information related to the body object.
 /// \param tol: Small number used to handle numerical approximation errors.
 void MoveIntersectingBodySoil(
-    SimOut* sim_out, Grid grid, Bucket* bucket, float tol);
+    SimOut* sim_out, Grid grid, Body* body, float tol);
 
 /// \brief This function moves the soil cells in the `terrain_` that intersect
-///        with a bucket.
+///        with a body.
 ///
 /// \param sim_out: Class that stores simulation outputs.
 /// \param tol: Small number used to handle numerical approximation errors.
 void MoveIntersectingBody(SimOut* sim_out, float tol);
 
-/// \brief This function tries to move the soil cells resting on the bucket
+/// \brief This function tries to move the soil cells resting on the body
 ///        layer `ind_p` at the location (`ii_p`, `jj_p`) to a new location
 ///        at (`ii_n`, `jj_n`).
 ///
 /// \param sim_out: Class that stores simulation outputs.
-/// \param ind_p: Index of the previous considered bucket layer.
+/// \param ind_p: Index of the previous considered body layer.
 /// \param ii_p: Index of the previous considered position in the X direction.
 /// \param jj_p: Index of the previous considered position in the Y direction.
 /// \param max_h: Maximum height authorized for the movement. [m]
@@ -55,21 +55,21 @@ void MoveIntersectingBody(SimOut* sim_out, float tol);
 /// \param h_soil: Height of the soil column left to be moved. [m]
 /// \param wall_presence: Indicates whether a wall is blocking the movement.
 /// \param grid: Class that stores information related to the simulation grid.
-/// \param bucket: Class that stores information related to the bucket object.
+/// \param body: Class that stores information related to the body object.
 /// \param tol: Small number used to handle numerical approximation errors.
 ///
-/// \return A tuple composed of the index of the new considered bucket layer,
+/// \return A tuple composed of the index of the new considered body layer,
 ///         the index of the new considered position in the X direction, the
 ///         index of the new considered position in the Y direction, the height
 ///         of the soil column left to be moved, and a boolean indicating
-///         whether a bucket wall is blocking the movement.
+///         whether a body wall is blocking the movement.
 std::tuple<int, int, int, float, bool> MoveBodySoil(
     SimOut* sim_out, int ind_p, int ii_p, int jj_p, float max_h, int ii_n,
-    int jj_n, float h_soil, bool wall_presence, Grid grid, Bucket* bucket,
+    int jj_n, float h_soil, bool wall_presence, Grid grid, Body* body,
     float tol);
 
 /// \brief This function identifies all the soil cells in the `terrain_` that
-///        intersect with the bucket.
+///        intersect with the body.
 ///
 /// \param sim_out: Class that stores simulation outputs.
 /// \param tol: Small number used to handle numerical approximation errors.
